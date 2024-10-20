6a972de570a6d80cf7fc993070514af225aba909
Aditya Shenoy K <a.shenoy.kasargod@accenture.com>
2024-10-20 23:27:34 +0530

    done

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,9 +9,9 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { updateInvoice } from "@/app/lib/actions";
 import { useActionState } from "react";
-
+import { updateInvoice, State } from '@/app/lib/actions';
+ 
 export default function EditInvoiceForm({
   invoice,
   customers,
